e357fb4d70fbb706134f58a4c4cde8711046d9f8
Budha Lantara <budhalantara@gmail.com>
2025-01-17 23:54:55 +0800

    setup vercel deployment

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -1,5 +1,14 @@
-import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapterNode from '@sveltejs/adapter-node';
+import adapterVercel from '@sveltejs/adapter-vercel';
+
+function customAdapter() {
+	if (process.env.PLATFORM_NODE) {
+		return adapterNode(arguments);
+	} else {
+		return adapterVercel(arguments);
+	}
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +20,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: customAdapter()
 	}
 };
 
